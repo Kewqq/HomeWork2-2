6fec7975d7f906dca7505abb1fcf748bf37f8cb1
Kewqq <rojer.007kew@gmail.com>
2024-09-13 11:58:10 +0700

    HomeWork2/2
    HomeWork2/2DiscountCard

---
+++ GoldCard.java
@@ -0,0 +1,12 @@
+public class GoldCard {
+    public GoldCard() {
+        super(0.03); // 3% discount
+    }
+
+    @Override
+    public double calculateFinalAmount(double amount) {
+        double effectiveDiscount = (amount >= 1000) ? getDiscount() : 0.0;
+        return amount * (1 - effectiveDiscount);
+    }
+}
+
---
+++ PlatinumCard.java
@@ -0,0 +1,11 @@
+public class PlatinumCard {
+    public PlatinumCard() {
+        super(0.05); // 5% discount
+    }
+
+    @Override
+    public double calculateFinalAmount(double amount) {
+        return amount * (1 - getDiscount());
+    }
+}
+
